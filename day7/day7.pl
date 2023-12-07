2eb4427311e2d56ebbb3bf3461810b85720bcc97
CDamianS <A01656277@tec.mx>
2023-12-07 16:20:45 -0600

    test

---
+++ day7/day7.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $sum = 0;
+
+my %world_value = ('A' => 13, 'K' => 12, 'Q' => 11, 'T' => 10,);
+
+while (<>) {
+    chomp;
+    my @matches = $_ =~ /(\d)/;
+
+}
